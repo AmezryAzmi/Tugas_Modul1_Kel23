75aab5eb4084ffd51334bbeb6fe044a8eff7a6eb
Indriawan Muhammad Akbar <63130587+silumankadal@users.noreply.github.com>
2022-10-06 16:49:01 +0700

    Add files via upload
    getsearch dan render

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+
+console.log(myData)
+
+
+function getSearch () {
+    const getInput = document.getElementById("inputText");
+
+    const element = myData.filter((el) =>
+        el.nama_lengkap.toLowerCase().includes(getInput.value.toLowerCase())
+    );
+
+    let data = document.getElementById("element-data")
+
+    if (getInput.value === '' || element.length === 0) {
+        data.innerHTML = 'There is nothing in here'
+    } else {
+        render(element)
+    }
+}
+
 function render(myData) {
     let data = document.getElementById('element-data')
 
