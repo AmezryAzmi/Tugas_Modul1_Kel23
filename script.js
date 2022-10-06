1df787dcfe540932038e3c8ca6e7d46b6ebeb3d7
Indriawan Muhammad Akbar <63130587+silumankadal@users.noreply.github.com>
2022-10-06 16:41:25 +0700

    Add files via upload
    function render

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
+function render(myData) {
+    let data = document.getElementById('element-data')
 
-console.log(myData)
+    data.innerHTML = ''
 
+    myData.forEach(item => {
+        let element =
+        `
+        <div class="item">
+            <h2>${item.nama_lengkap}</h2>
+            <p>Nama Panggilan : ${item.nama_panggilan} </p>
+            <p>Nomor Telepon : ${item.nomor_telepon} </p>
+            <p>id Line : ${item.id_line} </p>
+            <p>Tanggal Lahir : ${item.tanggal_lahir} </p>
+            <p>NIM : ${item.nim} </p>
+            <p>Email : ${item.email} </p>
+            <p>Hobi : ${item.hobi} </p>
+        </div>
+        `
 
-function getSearch () {
-    const getInput = document.getElementById("inputText");
+        data.insertAdjacentHTML("beforeend", element);
+    });
 
-    const element = myData.filter((el) =>
-        el.nama_lengkap.toLowerCase().includes(getInput.value.toLowerCase())
-    );
-
-    let data = document.getElementById("element-data")
-
-    if (getInput.value === '' || element.length === 0) {
-        data.innerHTML = 'There is nothing in here'
-    } else {
-        render(element)
-    }
 }
